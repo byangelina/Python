22d8cf9dc02c50bc7a3245b0fd12c479ddc5da47
Angelina <110882097+byangelina@users.noreply.github.com>
2025-11-17 13:45:51 -0300

    Add Base64 encoding example to Ejercicio1.py
    Added a Base64 encoding example and explanation.

--- Clase 21/Ejercicio1.py
+++ Clase 21/Ejercicio1.py
@@ -1,4 +1,18 @@
 # aprender a usar base64: caracteres que no tienen caracteres que no son universales
+"""
+se codifica en Base64 como sigue:
+
+TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz
+IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg
+dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu
+dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo
+ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=
+En la cita de arriba el valor codificado de Man es TWFu. Codificadas en ASCII, las letras: M, a y n son almacenadas como los bytes 77, 97 y 110, es decir, 01001101, 01100001, 01101110 en base 2.
+
+"""
+
+
+
 
 
 # Ejemplo 1
