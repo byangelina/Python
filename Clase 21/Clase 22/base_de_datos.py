7f9f4a87b0f74851c18b2b8b1170e52799276e18
Angelina <110882097+byangelina@users.noreply.github.com>
2025-11-24 16:35:47 -0300

    Create base_de_datos.py for SQLite database setup

---
+++ Clase 21/Clase 22/base_de_datos.py
@@ -0,0 +1,27 @@
+
+import sqlite3
+
+def conexion():
+        return sqlite3.connect("producto.db")
+
+
+def tabla_producto():
+        try:
+                conn = sqlite3.connect("producto.db")
+                cursor = conn.cursor()
+
+                cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS producto (
+                                codigo INTEGER PRIMARY KEY,
+                                descripcion TEXT,
+                                stock INTEGER,
+                                ubicacion TEXT
+                        )
+                """)
+                conn.commit()
+
+        except sqlite3.Error as x:
+                print("Error al crear tabla:", x)
+
+        finally:
+                conn.close()
